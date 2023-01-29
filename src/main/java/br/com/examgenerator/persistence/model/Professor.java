e250d0ebbf81e2058d6171e78a9658baa608decb
Dayvidson Veiga <dayvidsonveiga@gmail.com>
2023-01-29 01:16:54 -0300

    Professor and ApplicationUser entities created

---
+++ src/main/java/br/com/examgenerator/persistence/model/Professor.java
@@ -0,0 +1,23 @@
+package br.com.examgenerator.persistence.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@Entity
+public class Professor extends AbstractEntity{
+
+    @NotEmpty(message = "The field name cannot be empty")
+    private String name;
+
+    @Email
+    @NotEmpty(message = "The field email cannot be empty")
+    @Column(unique = true)
+    private String email;
+}
